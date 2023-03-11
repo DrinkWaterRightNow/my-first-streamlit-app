cb5164ed26fa0a5ee6869a52c16033ba8d01ac8f
DrinkWaterRightNow <125685035+DrinkWaterRightNow@users.noreply.github.com>
2023-03-11 14:34:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('Tham số a',0.00)
 b = st.number_input('Tham số b',0.00)
 if st.button('Giải'):
-    x = -b/a
-    st.success(f'Phương trình có 1 nghiệm: {x}')
+    if a == 0 and b == 0:
+        st.success('yeah')
+    else:
+        x = -b/a
+        st.success(f'Phương trình có 1 nghiệm: {x}')
 
