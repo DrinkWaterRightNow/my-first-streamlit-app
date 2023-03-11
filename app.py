8c9f5c7c8eac7c4da1e6be8bda260934b7857a11
DrinkWaterRightNow <125685035+DrinkWaterRightNow@users.noreply.github.com>
2023-03-11 14:32:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 
 import streamlit as st
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
-a = st.number_input('Tham số a',1)
-b = st.number_input('Tham số b',2)
+a = st.number_input('Tham số a',0.00)
+b = st.number_input('Tham số b',0.00)
 if st.button('Giải'):
     x = -b/a
     st.success(f'Phương trình có 1 nghiệm: {x}')
